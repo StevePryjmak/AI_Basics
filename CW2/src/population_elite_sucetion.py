c1502d8d2398fc3edd194e23a4bd640df37b40a1
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-11-14 22:38:18 +0100

    implementation of Population class

---
+++ CW2/src/population_elite_sucetion.py
@@ -0,0 +1,32 @@
+from constants import *
+from representant import Representant
+import numpy as np
+
+
+class Population:
+    def __init__ (self, size, func):
+        self.size = size
+        self.func = func
+        self.representants = [Representant(np.random.uniform(-UPPER_BOUND, UPPER_BOUND, DIMENSIONALITY), func) for _ in range(size)]
+
+    def get_best(self):
+        best = self.representants[0]
+        for rep in self.representants:
+            if rep.get_evaluation() < best.get_evaluation():
+                best = rep
+        return best
+    
+    def evolve(self, sigma=SIGMA):
+        new_population = []
+        new_population.append(self.get_best())
+        for _ in range(self.size - 1):
+            tournament = np.random.choice(self.representants, 2, replace=False)
+            best = min(tournament, key=lambda rep: rep.get_evaluation())
+            new_population.append(best.get_mutation(sigma))
+        self.representants = new_population
+    
+    def get_points(self):
+        return [rep.get_position() for rep in self.representants]
+
+    def get_values(self):
+        return [rep.get_evaluation() for rep in self.representants]
